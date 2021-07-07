5632c92c0ce8c9f78617daf929dd3f1dd5e91713
Paweesuda-Mim <workart53@gmail.com>
2021-07-07 19:48:32 +0700

    Add files

--- deployja/settings.py
+++ deployja/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from os
-from django_heroku
+import os
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
